85c356437322db18d29c807fc216b93956b38fce
Yossec <yosec.arratea22@gmail.com>
2024-08-28 08:54:43 -0500

    Cambios en el diseño, se modifico el diseño para el escritorio

--- views/vistas/Start.xaml.cs
+++ views/vistas/Start.xaml.cs
@@ -7,13 +7,6 @@ public partial class Start : ContentPage
 		InitializeComponent();
 	}
 
-    private async void Button_Clicked(object sender, EventArgs e)
-    {
-        var nuevaPagina = new Login();
-        await Navigation.PushAsync(nuevaPagina);
-
-        Navigation.RemovePage(this);
-    }
     private async void OnFrameTapped(object sender, EventArgs e)
     {
         var nuevaPagina = new Login();
