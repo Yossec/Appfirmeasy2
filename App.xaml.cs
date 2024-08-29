e245636d31e2a42c596f11d4c8288251dffeb996
Yossec <yosec.arratea22@gmail.com>
2024-08-29 12:23:26 -0500

    cambios empleados

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,7 +8,7 @@ namespace Appfirmeasy
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new NewPage1());
+            MainPage = new NavigationPage(new Start());
         }
     }
 }
--- views/vistas/NewPage1.xaml.cs
+++ views/vistas/NewPage1.xaml.cs
@@ -7,8 +7,6 @@ public partial class NewPage1 : ContentPage
 		InitializeComponent();
 		
 	}
-
-<<<<<<< HEAD
     private async void OnFrameTapped(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Registro());
@@ -17,10 +15,5 @@ public partial class NewPage1 : ContentPage
     private async void OnLabelRegistro(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Registro());
-=======
-    private void Button_Clicked(object sender, EventArgs e)
-    {
->>>>>>> 6f4993fdefcd19190bbdd8d152d2a7c13c64db12
-
     }
 }
